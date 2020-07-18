b8f1c669cacd23c945856fbd7f5739cd79bf6273
Jansen Fink <jansenfink@charter.net>
2020-07-18 20:31:13 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,7 +10,7 @@ def third_challenge
         :age => "54"
         },
       :hero => {
-        #your key/value pairs here
+        name: "Romeo", age: "15", status: "alive"
         },
       :hero_friends => []
    }, 
